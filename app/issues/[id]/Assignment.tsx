399d5cdb953354ffa3f4693dc8f10bf047851385
willphan <phanthanhnha1712000@gmail.com>
2024-10-31 21:08:55 -0400

    Populate assginee select component

--- app/issues/[id]/Assignment.tsx
+++ app/issues/[id]/Assignment.tsx
@@ -1,14 +1,31 @@
+'use client'
+
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Assignment = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get<User[]>('/api/users')
+      setUsers(data)
+    }
+
+    fetchUsers()
+  }, [])
+
   return (
     <Select.Root>
         <Select.Trigger placeholder='assign to ...'/>
         <Select.Content>
             <Select.Group>
                 <Select.Label>Suggestions</Select.Label>
-                <Select.Item value="1">Will</Select.Item>
-                <Select.Item value="2">Mosh</Select.Item>
+                {
+                  users.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)
+                }
             </Select.Group>
         </Select.Content>
     </Select.Root>
