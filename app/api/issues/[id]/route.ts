0f701422896c8ba878f8a8b9ab488a88751bf0b8
willphan <phanthanhnha1712000@gmail.com>
2024-11-06 14:33:35 -0500

    implement api issue assignment and change validation schema

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,16 +1,16 @@
 import authOptions from "@/app/auth/AuthOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { issueSchema, patchIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, {params}: {params: {id: string}}) {
-    const session = await getServerSession(authOptions)
-    if(!session)
-        return NextResponse.json({}, {status: 401})
+    // const session = await getServerSession(authOptions)
+    // if(!session)
+    //     return NextResponse.json({}, {status: 401})
     
     const body = await request.json()
-    const validation = issueSchema.safeParse(body)
+    const validation = patchIssueSchema.safeParse(body)
 
     if(!validation.success)
         return NextResponse.json(validation.error.format(), {status: 400})
@@ -22,11 +22,24 @@ export async function PATCH(request: NextRequest, {params}: {params: {id: string
     if(!issue)
         return NextResponse.json({error: 'Invalid issue'}, {status: 404})
 
+    const {title, description, assignment} = body;
+    if(assignment) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: assignment
+            }
+        })
+        if(!user) {
+            return NextResponse.json({ error: 'Invalid user'}, {status: 400})
+        }
+    }
+
     const updatedIssue = await prisma.issue.update({
         where: {id : issue.id},
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignmentId: assignment
         }
     })
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,10 @@ import {z} from 'zod';
 
 export const issueSchema = z.object({
     title: z.string().min(1).max(255),
-    description: z.string().min(1, "This is required"),
+    description: z.string().min(1, "This is required").max(65535),
+})
+export const patchIssueSchema = z.object({
+    title: z.string().min(1).max(255).optional(),
+    description: z.string().min(1, "This is required").max(65535).optional(),
+    assignment: z.string().min(1, 'Assignment is required').max(255).optional().nullable()
 })
