32898e90e66d902835c6e638fca99cf86c1e638b
willphan <phanthanhnha1712000@gmail.com>
2024-11-12 16:31:10 -0500

    implement filter issues by status

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,9 +2,18 @@ import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import Action from './Action';
 import{IssueStatusBadge, Link} from '@/app/components'
+import { Status } from '@prisma/client';
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({searchParams}: { searchParams: {status: Status} }) => {
+
+  const statuses = Object.values(Status)
+  const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+
+  const issues = await prisma.issue.findMany({
+    where : {
+      status: status
+    }
+  });
   
   return (
     <div>
