80c1caafc8bdccccf7efd07e1b50d142820d7634
willphan <phanthanhnha1712000@gmail.com>
2024-11-12 16:52:38 -0500

    make columns sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,18 @@
+import { IssueStatusBadge, Link } from '@/app/components';
 import prisma from '@/prisma/client';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 import { Table } from '@radix-ui/themes';
+import NextLink from 'next/link';
 import Action from './Action';
-import{IssueStatusBadge, Link} from '@/app/components'
-import { Status } from '@prisma/client';
 
-const IssuesPage = async ({searchParams}: { searchParams: {status: Status} }) => {
+const IssuesPage = async ({searchParams}: { searchParams: {status: Status, orderBy: keyof Issue} }) => {
+
+  const columns: {label: string, value: keyof Issue, className?: string}[] = [
+    {label: 'Issue', value: 'title'},
+    {label: 'Status', value: 'status', className: 'hidden md:table-cell'},
+    {label: 'Created', value: 'createdAt', className: 'hidden md:table-cell'},
+  ]
 
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
@@ -21,9 +29,12 @@ const IssuesPage = async ({searchParams}: { searchParams: {status: Status} }) =>
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created At</Table.ColumnHeaderCell>
+            {columns.map(column => <Table.ColumnHeaderCell key={column.value} className={column.className}>
+              <NextLink href={{
+                query: {...searchParams, orderBy: column.value}
+              }}>{column.label}</NextLink>
+              {column.value === searchParams.orderBy && <ArrowUpIcon className='inline'/>}
+            </Table.ColumnHeaderCell>)}
           </Table.Row>
         </Table.Header>
         <Table.Body>
