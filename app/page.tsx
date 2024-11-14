901823021a7d70f89c074e17e61038fb99555dee
willphan <phanthanhnha1712000@gmail.com>
2024-11-14 16:07:59 -0500

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Pagination from './issues/_components/Pagination'
 
 export default function Home() {
   return (
-    <div>Hello Guys, Welcome to Issue Tracker Page</div>
+    <Pagination itemCount={100} pageSize={10} currentPage={8}/>
   )
 }
